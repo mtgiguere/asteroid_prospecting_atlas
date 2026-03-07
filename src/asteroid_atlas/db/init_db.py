4a2d081c7e9d443bd4602ce3557f181dc4851ae7
Matt <mgiguere@marathonts.com>
2026-03-07 14:37:14 -0600

    ruff fix

--- src/asteroid_atlas/db/init_db.py
+++ src/asteroid_atlas/db/init_db.py
@@ -2,11 +2,10 @@
 init_db.py
 """
 
-from asteroid_atlas.db.session import engine
-from asteroid_atlas.models.base import Base
-
 import asteroid_atlas.models.asteroid  # noqa: F401
 import asteroid_atlas.models.asteroid_orbit  # noqa: F401
+from asteroid_atlas.db.session import engine
+from asteroid_atlas.models.base import Base
 
 
 def main() -> None:
