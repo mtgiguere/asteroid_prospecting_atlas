f9480cd6fdbdfff49c0bfacb6ec6196f1ec78f75
Matt <mgiguere@marathonts.com>
2026-05-20 14:41:53 -0500

    ruff format

--- tests/analytics/test_asteroid_visualization_query.py
+++ tests/analytics/test_asteroid_visualization_query.py
@@ -115,9 +115,9 @@ def test_list_asteroids_for_visualization_earth_crossing_filter():
     session.query(AsteroidOrbit).filter(
         AsteroidOrbit.asteroid_id.in_([crossing.id, non_crossing.id])
     ).delete(synchronize_session=False)
-    session.query(Asteroid).filter(
-        Asteroid.nasa_jpl_id.in_([crossing_id, non_crossing_id])
-    ).delete(synchronize_session=False)
+    session.query(Asteroid).filter(Asteroid.nasa_jpl_id.in_([crossing_id, non_crossing_id])).delete(
+        synchronize_session=False
+    )
     session.commit()
     session.close()
 
